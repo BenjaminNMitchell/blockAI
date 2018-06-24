69ed269103774c0de0bfc2605103eed5668913c2
Ben Mitchell <benjamin.neil.mitchell@gmail.com>
2018-06-24 18:15:17 -0400

    Added testing for point class

--- block_ai/lib/myblokus/point.py
+++ block_ai/lib/myblokus/point.py
@@ -1,49 +1,86 @@
+import itertools
+
 class Point:
     """Holds x and y cordinates"""
 
-    def __init__(self, x, y):
+    def __init__(self, x: int, y: int) -> None:
         self.x = x
         self.y = y
+
+    @property
+    def x(self) -> int:
+        """The x coordinate of this point."""
+
+        return self.__x
+
+    @x.setter
+    def x(self, x: int):
+        """Setter for the x coordinate of this point."""
+
+        if type(x) is not int:
+            raise TypeError("x must be an int")
+        self.__x = x
+
+    @property
+    def y(self) -> int:
+        """The y coordinate of this point."""
+
+        return self.__y
+
+    @y.setter
+    def y(self, y: int):
+        """Setter for the y coordinate of this point."""
+
+        if type(y) is not int:
+            raise TypeError("y must be an int")
+        self.__y = y
     
+
     def get_adjacent(self):
-        return [Point(self.x, self.y + 1),
+        """Get all points at distance one."""
+
+        return [
+                Point(self.x - 1, self.y),
                 Point(self.x, self.y - 1),
-                Point(self.x + 1, self.y),
-                Point(self.x - 1, self.y)]
-        
+                Point(self.x, self.y + 1),
+                Point(self.x + 1, self.y)
+            ]
+
     def get_corners(self):
-        gen = itertools.product([1, -1], [1, -1])
+        """Get all diagonal points."""
+
+        gen = itertools.product([-1, 1], [-1, 1])
         return [Point(self.x + a, self.y + b) for a, b in gen]
-            
-    def __str__(self):
+
+    def __hash__(self) -> int:
+        return hash((self.x, self.y))
+
+    def __eq__(self, other) -> bool:
+        return self.x == other.x and self.y == other.y
+
+    def __ne__(self, other) -> bool:
+        return not self == other
+
+    def __lt__(self, other) -> bool:
+        if self == other:
+            return True
+        elif self.x == other.x:
+            return self.y < other.y
+        else:
+            return self.x < other.x
+
+    def __gt__(self, other) -> bool:
+        return not self < other
+
+    def __add__(self, other):
+        return Point(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other):
+        return Point(self.x - other.x, self.y - other.y)
+
+    def __str__(self) -> str:
         return f"({self.x}, {self.y})"
 
     def __repr__(self):
         return str(self)
-
-    def __eq__(self, other):
-        return self.x == other.x and self.y == other.y
-
-    def __hash__(self):
-        return hash((self.x, self.y))
-
-    def __gt__(self, other):
-        if self == other:
-            return True
-        elif self.x + self.y > other.x + other.y:
-            return True
-        else:
-            if self.x == other.x:
-                return self.y > other.y
-            else:
-                return self.x > self.y
-            
-    def __add__(self, other):
-        return Point(self.x + other.x, self.y + other.y)
-    
-    def __sub__(self, other):
-        return Point(self.x - other.x, self.y - other.y)
-            
-    def __lt__(self, other):
-        return self.__gt__(other)
 
--- test/lib/myblokus/test_point.py
+++ test/lib/myblokus/test_point.py
@@ -1,9 +1,135 @@
 from block_ai.lib.myblokus.point import Point
+
 import unittest
+
+from hypothesis import given
+import hypothesis.strategies as st
 
 class PointTests(unittest.TestCase):
 
-    def test_hello_world(self):
-        print("Hello world")
-        self.assertTrue(True)
+    def test_init(self):
+        p = Point(1, 1)
+
+    def test_init_fails(self):
+        with self.assertRaises(TypeError):
+            p = Point(None, None)
+
+    def test_get_adjacent(self):
+        # Given 
+        p = Point(1, 1)
+
+        # When
+        adjacent = p.get_adjacent()
+
+        # Then
+        expected = [Point(0, 1),
+                    Point(1, 0),
+                    Point(1, 2),
+                    Point(2, 1)]
+        self.assertEqual(adjacent, expected)
+
+    def test_get_corners(self):
+        # Given
+        p = Point(1, 1)
+
+        # When
+        corners = p.get_corners()
+
+        # Then
+        expected = [Point(0, 0),
+                    Point(0, 2),
+                    Point(2, 0),
+                    Point(2, 2)]
+        self.assertTrue(corners, expected)
+
+    def test_str(self):
+        # Given
+        p = Point(1, 1)
+
+        # When
+        p_str = str(p)
+
+        # Then
+        self.assertEqual(p_str, "(1, 1)")
+
+    @given(x=st.integers(), y=st.integers())
+    def test_equal(self, x, y):
+        p1 = Point(x, y)
+        p2 = Point(x, y)
+
+        self.assertTrue(p1 == p2)
+
+
+    @given(x=st.integers(), y=st.integers())
+    def test_not_equal(self, x, y):
+        p1 = Point(x, y)
+        p2 = Point(x + 1, y)
+        
+        self.assertNotEqual(p1, p2)
+        self.assertTrue(p1 != p2)
+    
+    @given(x=st.integers(), y=st.integers())
+    def test_hash(self, x, y):
+        p1 = Point(x, y)
+        p2 = Point(x, y)
+    
+        self.assertEqual(hash(p1), hash(p2))
+
+    @given(x=st.integers(), y=st.integers())
+    def test_less_than(self, x, y):
+        # Given
+        p1 = Point(x - 1, y)
+        p2 = Point(x, y - 1)
+        p3 = Point(x, y)
+        p4 = Point(x, y + 1)
+        p5 = Point(x + 1, y + 1)
+
+        # Then
+        self.assertTrue(p1 < p2)
+        self.assertTrue(p2 < p3)
+        self.assertTrue(p3 < p4)
+        self.assertTrue(p4 < p5)
+
+    
+    @given(x=st.integers(), y=st.integers())
+    def test_greater_than(self, x, y):
+        # Given
+        p1 = Point(x - 1, y)
+        p2 = Point(x, y - 1)
+        p3 = Point(x, y)
+        p4 = Point(x, y + 1)
+        p5 = Point(x + 1, y + 1)
+
+        # Then
+        self.assertFalse(p1 > p2)
+        self.assertFalse(p2 > p3)
+        self.assertFalse(p3 > p4)
+        self.assertFalse(p4 > p5)
+
+    @given(x=st.integers(), y=st.integers(), a=st.integers(), b=st.integers())
+    def test_add(self, x, y, a, b):
+        # Given
+        p1 = Point(x, y)
+        p2 = Point(a, b)
+
+        # When
+        p_sum = p1 + p2
+
+        # Then
+        self.assertEqual(p_sum.x, x + a)
+        self.assertEqual(p_sum.y, y + b)
+
+
+    @given(x=st.integers(), y=st.integers(), a=st.integers(), b=st.integers())
+    def test_sub(self, x, y, a, b):
+        # Given
+        p1 = Point(x, y)
+        p2 = Point(a, b)
+        
+        # When
+        p_sum = p1 - p2
+
+        # Then
+        self.assertEqual(p_sum.x, x - a)
+        self.assertEqual(p_sum.y, y - b)
 
