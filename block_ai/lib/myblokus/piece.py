5f93519e77f46d940ac937818aa4291b758f2835
Owen Mitchell <owenrossmitchell@gmail.com>
2018-07-06 00:42:54 -0400

    added all 5 unit pieces

--- block_ai/lib/myblokus/piece.py
+++ block_ai/lib/myblokus/piece.py
@@ -60,23 +60,37 @@ class Piece:
         return f"Piece({str(self.orientations)})"
 
 def gen_pieces():
-    """ Generates the pieces available to a player at the start of a game """
-
+    """Generates the pieces available to a player at the start of a game"""
+    # Numbered according to graphic included in
     p1 = Piece([Point(0, 0)])
-    p2 = Piece([Point(0, 0), Point(0, 1)])
-    p3_1 = Piece([Point(0, 0), Point(0, 1), Point(0, 2)])
-    p3_2 = Piece([Point(0, 0), Point(0, 1), Point(1, 1)])
-    p4_1 = Piece([Point(0, 0), Point(1, 0), Point(0, 1), Point(1, 1)])
-    p4_2 = Piece([Point(0, 0), Point(1, 0), Point(2, 0), Point(3, 0)])
-    p4_3 = Piece([Point(0, 0), Point(1, 0), Point(1, 1), Point(2, 1)])
-    p4_4 = Piece([Point(0, 0), Point(1, 0), Point(1, 1), Point(2, 0)])
-    
-    # TODO implement remaining Pieces and add to gen_pieces
-    all_pieces = [
-        p1,
-        p2,
-        p3_1, p3_2,
-        p4_1, p4_2, p4_3, p4_4
-    ]
-    return all_pieces
 
+    p2 = Piece([Point(0, 0), Point(1, 0)])
+
+    p3 = Piece([Point(0, 0), Point(1, 0), Point(2, 0)])
+    p4 = Piece([Point(0, 0), Point(1, 0), Point(1, 1)])
+
+    p5 = Piece([Point(0, 0), Point(1, 0), Point(2, 0), Point(3, 0)])
+    p6 = Piece([Point(0, 0), Point(1, 0), Point(2, 0), Point(2, 1)])
+    p7 = Piece([Point(0, 0), Point(1, 0), Point(2, 0), Point(1, 1)])
+    p8 = Piece([Point(0, 0), Point(1, 0), Point(0, 1), Point(1, 1)])
+    p9 = Piece([Point(0, 0), Point(1, 0), Point(1, 1), Point(2, 1)]
+
+    p10 = Piece([Point(0, 0), Point(1, 0), Point(2, 0), Point(3, 0)], Point(4, 0))
+    p11 = Piece([Point(0, 0), Point(1, 0), Point(2, 0), Point(3, 0)], Point(3, 1))
+    p15 = Piece([Point(0, 0), Point(1, 0), Point(2, 0), Point(3, 0)], Point(2, 1))
+    p12 = Piece([Point(0, 0), Point(1, 0), Point(2, 0), Point(2, 1)], Point(3, 1))
+    p17 = Piece([Point(0, 0), Point(1, 0), Point(2, 0), Point(2, 1)], Point(2, 2))
+    p16 = Piece([Point(0, 0), Point(1, 0), Point(2, 0), Point(1, 1)], Point(1, 2))
+    p13 = Piece([Point(0, 0), Point(1, 0), Point(2, 0), Point(1, 1)], Point(2, 1))
+    p18 = Piece([Point(0, 0), Point(1, 0), Point(1, 1), Point(2, 1)], Point(2, 2))
+    p20 = Piece([Point(0, 0), Point(1, 0), Point(1, 1), Point(2, 1)], Point(1, 2))
+    p21 = Piece([Point(0, 1), Point(1, 1), Point(1, 2), Point(1, 0)], Point(2, 1))
+    p14 = Piece([Point(0, 0), Point(1, 0), Point(2, 0), Point(0, 1)], Point(2, 1))
+    p19 = Piece([Point(0, 0), Point(0, 1), Point(1, 1), Point(2, 1)], Point(2, 2))
+
+    return [p1,
+            p2,
+            p3, p4,
+            p5, p6, p7, p8, p9,
+            p10, p11, p12, p13, p14, p15, p16, p17, p18, p19, p20, p21
+           ]
