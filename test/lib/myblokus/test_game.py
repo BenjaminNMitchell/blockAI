f80e26669f57cf7769d97e68cf2e8336e2b626a2
Ben Mitchell <benjamin.neil.mitchell@gmail.com>
2018-09-17 14:28:38 -0400

    WIP Started test for exact move options at each position

--- test/lib/myblokus/test_game.py
+++ test/lib/myblokus/test_game.py
@@ -1,4 +1,5 @@
 from block_ai.lib.myblokus.game import Game
+from . import expected_move import expected_moves
 import unittest
 
 
@@ -6,6 +7,11 @@ class GameTests(unittest.TestCase):
 
     def test_game(self):
         # Given
-
         game = Game()
+
+        # When
+
+        for val in expected_moves:
+            move, options = expected_moves[val]
+            
 
